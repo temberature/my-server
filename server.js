b713d0c10dbcd965f0fa2cd9c4d715bf1b7151b6
Tong <temberature@gmail.com>
2023-12-25 21:57:48 +0800

    * feat(server.js): add support for user authentication and authorization * feat(server.js): add support for user signup * feat(server.js): add support for user signin * feat(server.js): add support for fetching user information * fix(server.js): fix error handling in /upload endpoint * fix(server.js): fix error handling in /feeds endpoint * fix(server.js): fix error handling in handleOpmlFile function

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 const fs = require('fs');
 const { parseString } = require('xml2js');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
+
+// 用动态导入替换原来的 require
+let fetch;
+
+(async () => {
+    fetch = (await import('node-fetch')).default;
+})();
+
+// 确保在 fetch 初始化之后使用它
+
 
 const supabaseUrl = process.env.supabaseUrl;
 const supabaseKey = process.env.supabaseKey;
@@ -17,24 +28,50 @@ app.use(bodyParser.json());
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
+// Helper function to decode JWT and get user ID
+const getUserIdFromToken = (token) => {
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        return decoded.sub;
+    } catch (error) {
+        if (error instanceof jwt.TokenExpiredError) {
+            // Handle the specific case of an expired token
+            return null;
+        }
+        throw error; // Re-throw other unexpected errors
+    }
+};
+
 app.post('/upload', upload.single('file'), async (req, res) => {
+    console.log(req.headers);
     if (req.file) {
         const filePath = req.file.path;
         const fileContent = fs.readFileSync(filePath, 'utf8');
+        const userToken = req.headers.authorization.split(' ')[1];
+        const userId = getUserIdFromToken(userToken);
+        console.log(userId)
+        if (!userId) {
+            return res.status(401).send('Unauthorized');
+        }
 
         try {
             const feeds = await handleOpmlFile(fileContent);
-            const { data, error } = await supabase.from('feeds').insert(feeds);
-
-            if (error) {
-                console.error('Error uploading data to Supabase:', error);
-                res.status(500).send('Error uploading data to Supabase');
-            } else {
-                res.send({ message: 'File processed successfully', data });
+            if (!Array.isArray(feeds)) {
+                throw new Error("handleOpmlFile did not return an array");
             }
+    
+            // Now use .map as feeds is confirmed to be an array
+            const feedsData = feeds.map(feed => ({
+                ...feed,
+                user_id: userId
+            }));
+            const { data, error } = await supabase.from('feeds').insert(feedsData);
+            if (error) throw error;
+
+            res.send({ message: 'File processed successfully', data });
         } catch (error) {
-            console.error('Error parsing OPML file:', error);
-            res.status(500).send('Error parsing OPML file');
+            console.error('Error:', error);
+            res.status(500).send('Server Error');
         } finally {
             fs.unlinkSync(filePath);
         }
@@ -44,15 +81,80 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 });
 
 app.get('/feeds', async (req, res) => {
+    const userToken = req.headers.authorization.split(' ')[1];
+    const userId = getUserIdFromToken(userToken);
+
+    if (!userId) {
+        return res.status(401).send('Unauthorized');
+    }
+
     const { data, error } = await supabase
         .from('feeds')
-        .select('*');
+        .select('*')
+        .eq('user_id', userId);
 
     if (error) {
-        console.error('Error fetching feeds:', error);
-        res.status(500).send('Error fetching feeds');
+        console.error('Error:', error);
+        res.status(500).send('Server Error');
     } else {
         res.json(data);
+    }
+});
+// 注册新用户
+app.post('/auth/signup', async (req, res) => {
+    const { email, password } = req.body;
+    const { user, error } = await supabase.auth.signUp({ email, password });
+
+    if (error) {
+        return res.status(401).json({ error: error.message });
+    }
+
+    res.json({ user });
+});
+
+// 用户登录
+app.post('/auth/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const response = await fetch(`${supabaseUrl}/auth/v1/token?grant_type=password`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'apikey': supabaseKey,
+                'Authorization': `Bearer ${supabaseKey}`
+            },
+            body: JSON.stringify({ email, password })
+        });
+        const data = await response.json();
+
+        if (data.error) {
+            res.status(401).json({ error: data.error.message });
+        } else {
+            res.json({ user: data.user, session: data.access_token });
+        }
+    } catch (error) {
+        console.error('Login error:', error);
+        res.status(500).send('Internal server error');
+    }
+});
+// 获取用户信息
+app.get('/auth/user', (req, res) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).send('No token provided');
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        res.json({ user: decoded }); // Send decoded token data as user info
+    } catch (error) {
+        if (error instanceof jwt.TokenExpiredError) {
+            res.status(401).send('Token expired');
+        } else {
+            res.status(401).send('Invalid token');
+        }
     }
 });
 
@@ -83,6 +185,7 @@ function handleOpmlFile(fileContent) {
                 reject(err);
             } else {
                 const feeds = parseOpmlToJson(result);
+                console.log("Feeds:", feeds); // Check the output here
                 resolve(feeds);
             }
         });
